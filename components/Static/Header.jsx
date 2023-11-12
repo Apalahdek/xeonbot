6a1b49f61a77b9b21a4a3d0f387153adb32c63e5
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2023-11-12 21:06:45 +0700

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -166,7 +166,7 @@ const Header = ({ $, NavItems }) => {
                                 </Menu.Items>
                             </Transition>
                         </Menu>
-                        <Link href="https://wa.me/19096108238">
+                        <Link href="https://wa.me/13235463246">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
                                Invite
                             </a>
