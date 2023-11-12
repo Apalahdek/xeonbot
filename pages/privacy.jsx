84b55790bdaa26e74ff75139da7500958213e3cc
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2023-11-12 10:55:08 +0700

    Add files via upload

---
+++ pages/privacy.jsx
@@ -0,0 +1,23 @@
+export default function Privacy() {
+
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for xᴇᴏɴ´</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2023  🆕️</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+        <p>⊳ To use the xᴇᴏɴ´, you must define the necessary permissions when adding it to your whatsapp group. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <div className="py-3"></div>
+        <p>⊳ You have to chat <a className="text-white" href="https://wa.me/6285701479245">Owner</a> xᴇᴏɴ´ when using certain commands from the bot commands.</p>
+        <div className="py-3"></div>
+        <p>⊳ Every user can use the xᴇᴏɴ´ however they want, as long as they do not intend to harm.</p>
+        <div className="py-3"></div>
+        <p>⊳ You cannot use certain commands at the same time while using xᴇᴏɴ´.</p>   
+        <div className="py-3"></div>
+        <p>⊳ xᴇᴏɴ´ does not save any data based on users.</p>
+        <div className="py-3"></div>
+        <p>⊳ While using the bot, your number is saved and deleted in the system for 60 seconds for once.</p> 
+    
+        </div>
+            </div>
+    );
+};
